25a68c1032472cf5a31c587740db46fb32bd187d
Muhammad Safeer <64736313+m-safeer@users.noreply.github.com>
2020-07-28 17:07:35 +0500

    Todo App Assignment Completed

--- script.js
+++ script.js
@@ -1,54 +1,106 @@
 var list = document.getElementById("list");
 
-    function addTodo(){
-    
-        var todoInput = document.getElementById("todo");
-        if(todoInput.value.length > 0){
+function addTodo(){
+    var todoInput = document.getElementById("todo");
+    if(todoInput.value.length > 0){
 
-            // List Creation
-        var li = document.createElement("li");
-        var liText = document.createTextNode(todoInput.value);
-        li.setAttribute("class","my-3")
-        li.appendChild(liText);
-        list.appendChild(li);
+    // List Creation
+    var li = document.createElement("li");
+    li.setAttribute("class","my-3");
+    var p = document.createElement("p")
+    p.setAttribute("class","itempara");
+    var liText = document.createTextNode(todoInput.value);
+    list.appendChild(li);
+    li.appendChild(p);
+    p.appendChild(liText)
 
-        // List Delete Button
-        var delbtn = document.createElement("button");
-        var delbtnText = document.createTextNode("Delete");
-        delbtn.appendChild(delbtnText);
-        delbtn.setAttribute("onclick","delList(this)");
-        delbtn.setAttribute("class"," btn btn-success btn-sm mx-1")
-        li.appendChild(delbtn);
+    // Edit Button Creation
+    var editBtn = document.createElement("i");
+    editBtn.setAttribute("onclick","editItem(this)");
+    editBtn.setAttribute("class","fa fa-pencil-square-o fa-2x text-success mx-2")
+    li.appendChild(editBtn);
 
-        // List Edit Button
-        var editbtn = document.createElement("button");
-        var editbtnText = document.createTextNode("Edit");
-        editbtn.appendChild(editbtnText);
-        editbtn.setAttribute("onclick","editItem(this)");
-        editbtn.setAttribute("class","btn btn-success btn-sm mx-1")
-        li.appendChild(editbtn);
+    // Delete Button Creation
+    var delBtn = document.createElement("i");
+    delBtn.setAttribute("onclick","delList(this)");
+    delBtn.setAttribute("class","fa fa-trash-o fa-2x text-success mx-2")
+    li.appendChild(delBtn);
 
-        todoInput.value = "";
-        }
-
-        else{
-            alert("Enter Something");
-        }
+    todoInput.value = "";
     }
 
-// Delete All Function
-function delAll(){
-    list.innerHTML ="";
+    else{
+        alert("Enter Todo Task First")
+       
+    }
 }
 
-// List Delete Button Function
-function delList(e){
-    e.parentNode.remove();
-}
+    // Delete All Function
+    function delAll(){
+        list.innerHTML ="";
+    }
 
-// List Edit Button Function
-function editItem(e){
-    var value = e.parentNode.firstChild.nodeValue;
-    var newVal = prompt("Enter", value);
-    e.parentNode.firstChild.nodeValue = newVal;
-}
+    // Delete Button Function
+    function delList(e){
+        e.parentNode.remove();
+    }
+
+    // Edit Button Function
+    function editItem(e){
+
+        // Clear Everything
+        var valueText = e.parentNode.firstChild.firstChild.nodeValue;
+        var value = e.parentNode;
+        value.innerHTML = "";
+
+        // Input Creation
+        var input = document.createElement("input");
+        input.setAttribute("type","text")
+        input.setAttribute("class", "form-control")
+
+        // Div Creation
+        var div = document.createElement("div")
+        div.setAttribute("class","itempara")
+
+        value.appendChild(div);
+        div.appendChild(input);
+        input.value = valueText;
+
+        // Update Button Creation
+        var upd_btn = document.createElement("i");
+            upd_btn.setAttribute("onclick","updItem(this)");
+            upd_btn.setAttribute("class","fa fa-check-circle-o fa-2x text-success mx-2")
+            value.appendChild(upd_btn);
+
+        // Cancel Button Creation
+        var cancelBtn = document.createElement("i")
+            cancelBtn.setAttribute("onclick","delList(this)");
+            cancelBtn.setAttribute("class","fa fa-trash-o fa-2x text-success mx-2");
+            value.appendChild(cancelBtn);
+    }
+
+        // Update Button Function
+        function updItem(e){
+            var upd_valText = e.parentNode.firstChild.firstChild.value;
+            var upd_val = e.parentNode;
+            upd_val.innerHTML = "";
+
+            // P Creation
+            var upd_p = document.createElement("p")
+            upd_p.setAttribute("class","itempara");
+            var upd_pText = document.createTextNode(upd_valText);
+            upd_val.appendChild(upd_p);
+            upd_p.appendChild(upd_pText)
+
+            // Edit Button Creation
+            var upd_editBtn = document.createElement("i");
+            upd_editBtn.setAttribute("onclick","editItem(this)");
+            upd_editBtn.setAttribute("class","fa fa-pencil-square-o fa-2x text-success mx-2")
+            upd_val.appendChild(upd_editBtn);
+
+            // Delete Button Creation
+            var upd_delBtn = document.createElement("i");
+            upd_delBtn.setAttribute("onclick","delList(this)");
+            upd_delBtn.setAttribute("class","fa fa-trash-o fa-2x text-success mx-2")
+            upd_val.appendChild(upd_delBtn);
+        }
